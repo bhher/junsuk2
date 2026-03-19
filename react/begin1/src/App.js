44f1077e263d01b88a75c0669361f7242f9c9de1
bhher30 <bhher30@gmail.com>
2026-03-19 17:00:40 +0900

    a1319

---
+++ react/begin1/src/App.js
@@ -0,0 +1,14 @@
+import Hello from "./Hello";
+import "./App.css";
+import Wrapper from "./Wrapper";
+
+function App() {
+  return (
+    <Wrapper>
+      <Hello name="react" color="red" isSpecial />
+      <Hello color="pink" />
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ react/begin1/src/React1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const React1 = () => {
+  return <div>react1</div>;
+};
+
+export default React1;
