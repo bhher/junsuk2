717c324dd42a4b3bcd2459e8c3d7d0cf08f6fe2f
bhher30 <bhher30@gmail.com>
2026-05-07 16:18:15 +0900

    a0507

--- spring/thymeleaf-examples/src/main/java/com/example/thymeleaf_examples/repository/DemoItemRepository.java
+++ spring/thymeleaf-examples/src/main/java/com/example/thymeleaf_examples/repository/DemoItemRepository.java
@@ -3,6 +3,8 @@ package com.example.thymeleaf_examples.repository;
 import com.example.thymeleaf_examples.domain.DemoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DemoItemRepository  extends JpaRepository<DemoItem, Long> {
+import java.util.List;
 
+public interface DemoItemRepository  extends JpaRepository<DemoItem, Long> {
+    List<DemoItem> findAllByOrderByIdAsc();
 }
